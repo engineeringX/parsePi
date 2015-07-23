c27883ded1cfb8f80fd520201c3f1ff8a6c0835a
Raunaq Sawhney <raunaq.sawhney19@gmail.com>
2015-07-23 19:58:49 -0400

    add file association to EventObject on parse

--- send-to-parse.py
+++ send-to-parse.py
@@ -1,10 +1,34 @@
 import json,httplib
+
+appID = "kKW7oJS0nwEG4V6f3LvYooU5BQxFnH6eZ9aS31A3"
+apiKey = "HEZHvUyEqV4VOV61YaEFbMywGKq7pJNlPhlQtWRt"
+uploadFilename = "samples.txt"
+
 connection = httplib.HTTPSConnection('api.parse.com', 443)
 connection.connect()
-connection.request('POST', '/1/files/hello.txt', 'Hello, World!', {
-       "X-Parse-Application-Id": "kKW7oJS0nwEG4V6f3LvYooU5BQxFnH6eZ9aS31A3",
-       "X-Parse-REST-API-Key": "HEZHvUyEqV4VOV61YaEFbMywGKq7pJNlPhlQtWRt",
+connection.request('POST', '/1/files/' + uploadFilename, open(uploadFilename, 'rb').read(), {
+       "X-Parse-Application-Id": appID,
+       "X-Parse-REST-API-Key": apiKey,
        "Content-Type": "text/plain"
      })
+
 result = json.loads(connection.getresponse().read())
-print result
+filename = result["name"]
+print filename + ' uploaded'
+
+#Uploaded the file to Parse
+#Now associate it with Event Object
+connection.request('POST','/1/classes/EventObject', json.dumps({
+    "name": "Sample",
+    "data": {
+        "name": filename,
+        "__type": "File"
+         }
+    }), {
+        "X-Parse-Application-Id": appID,
+        "X-Parse-REST-API-Key": apiKey,
+        "Content-Type": "applicaton/json"
+     }
+    )
+result = json.loads(connection.getresponse().read())
+print filename + ' associated with object ' + result["objectId"]
