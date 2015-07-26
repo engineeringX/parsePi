d7c303c123f85de3f8ff4e71b9b9e91e2d0f90f8
Simarpreet Singh <simar@linux.com>
2015-07-26 13:20:54 -0400

    adding sending by field type
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- bled112_scanner.py
+++ bled112_scanner.py
@@ -39,6 +39,18 @@ filter_mac = []
 filter_rssi = 0
 file_send_counter = 0
 
+# parse stuff
+accel_scaling = 2**15
+gyro_scaling = 131
+temp_scaling = 0.03125
+pulse_scaling = 1024
+appID = "kKW7oJS0nwEG4V6f3LvYooU5BQxFnH6eZ9aS31A3"
+apiKey = "HEZHvUyEqV4VOV61YaEFbMywGKq7pJNlPhlQtWRt"
+uploadFilename = "samples.txt"
+
+connection = httplib.HTTPSConnection('api.parse.com', 443)
+connection.connect()
+
 def main():
     global options, filter_uuid, filter_mac, filter_rssi, file_send_counter
 
@@ -405,7 +417,7 @@ def bgapi_parse(b):
 
                                 if this_field[0] == 0xFF: # manufactuerer specific data
                                     # LOL HAX
-                                   
+
                                     #it = iter(this_field[1:] + [0] if (len(this_field[1:]) & 0x1) else this_field[1:])
 				    it = iter(this_field[1:])
                                     ad_manufacturer = [((next(it) << 8) | x) for x in it]
@@ -415,7 +427,7 @@ def bgapi_parse(b):
                     # Print out just manufacturer data
                     data_list = [(x - 65536) if (x & 0x8000) else x for x in ad_manufacturer]
 		    print data_list
-		
+
 		global file_send_counter
 		file_send_counter += 1
 
@@ -465,39 +477,71 @@ def bgapi_parse(b):
 
         bgapi_rx_buffer = []
 
+def upload(upload_data):
+        connection.request('POST','/1/classes/EventObject', json.dumps({
+            "a_x": float(upload_data[0]),
+            "a_y": float(upload_data[1]),
+            "a_z": float(upload_data[2]),
+            "g_x": float(upload_data[3]),
+            "g_y": float(upload_data[4]),
+            "g_z": float(upload_data[5]),
+            "tmp": float(upload_data[6]),
+            "pul": float(upload_data[7])
+            }), {
+                "X-Parse-Application-Id": appID,
+                "X-Parse-REST-API-Key": apiKey,
+                "Content-Type": "applicaton/json"
+             })
+        result = json.loads(connection.getresponse().read())
+        #print result
+
+def parse_chunks(chunks):
+        chunks_length = len(chunks)
+
+        a_x_list = []
+        a_y_list = []
+        a_z_list = []
+        g_x_list = []
+        g_y_list = []
+        g_z_list = []
+        tmp_list = []
+        pul_list = []
+
+        # Arbitrary chunk size
+        chunk_size = chunks_length % 10
+
+        for i in xrange(0, chunks_length, chunk_size):
+                chunk = chunks[i: i + chunk_size];
+                for x in range(0, len(chunk)):
+                        a_x_list.append(float(chunk[x][0]))
+                        a_y_list.append(float(chunk[x][1]))
+                        a_z_list.append(float(chunk[x][2]))
+                        g_x_list.append(float(chunk[x][3]))
+                        g_y_list.append(float(chunk[x][4]))
+                        g_z_list.append(float(chunk[x][5]))
+                        tmp_list.append(float(chunk[x][6]))
+                        pul_list.append(float(chunk[x][7]))
+
+                a_x_avg = (sum(a_x_list)/float(len(a_x_list))) / accel_scaling
+                a_y_avg = (sum(a_y_list)/float(len(a_y_list))) / accel_scaling
+                a_z_avg = (sum(a_z_list)/float(len(a_z_list))) / accel_scaling
+                g_x_avg = (sum(g_x_list)/float(len(g_x_list))) / gyro_scaling
+                g_y_avg = (sum(g_y_list)/float(len(g_y_list))) / gyro_scaling
+                g_z_avg = (sum(g_z_list)/float(len(g_z_list))) / gyro_scaling
+                tmp_avg = (sum(tmp_list)/float(len(tmp_list))) * temp_scaling
+                pul_avg = (sum(pul_list)/float(len(pul_list))) / pulse_scaling
+
+                upload_data = [a_x_avg, a_y_avg, a_z_avg, g_x_avg, g_y_avg, g_z_avg, tmp_avg, pul_avg]
+                print upload_data
+                upload(upload_data)
+
 def sendtoparse():
-	appID = "kKW7oJS0nwEG4V6f3LvYooU5BQxFnH6eZ9aS31A3"
-	apiKey = "HEZHvUyEqV4VOV61YaEFbMywGKq7pJNlPhlQtWRt"
-	uploadFilename = "samples.txt"
-
-	connection = httplib.HTTPSConnection('api.parse.com', 443)
-	connection.connect()
-	connection.request('POST', '/1/files/' + uploadFilename, open(uploadFilename, 'rb').read(), {
-	       "X-Parse-Application-Id": appID,
-	       "X-Parse-REST-API-Key": apiKey,
-	       "Content-Type": "text/plain"
-	     })
-
-	result = json.loads(connection.getresponse().read())
-	filename = result["name"]
-	#print filename + ' uploaded'
-
-	#Uploaded the file to Parse
-	#Now associate it with Event Object
-	connection.request('POST','/1/classes/EventObject', json.dumps({
-	    "name": "Sample",
-	    "data": {
-		"name": filename,
-		"__type": "File"
-		 }
-	    }), {
-		"X-Parse-Application-Id": appID,
-		"X-Parse-REST-API-Key": apiKey,
-		"Content-Type": "applicaton/json"
-	     }
-	    )
-	result = json.loads(connection.getresponse().read())
-	#print filename + ' associated with object ' + result["objectId"]
+        inputlines = []
+        f = open('samples.txt')
+        for line in iter(f):
+                inputlines.append(line)
+        f.close()
+        parse_chunks(inputlines)
 
 # gracefully exit without a big exception message if possible
 def ctrl_c_handler(signal, frame):
